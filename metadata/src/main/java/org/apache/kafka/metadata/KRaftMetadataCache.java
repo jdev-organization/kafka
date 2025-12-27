681895163ea49989db45468855e318c92c26e9dd
lucaspan <panxiangyi@hotmail.com>
2025-12-27 15:23:12 -0500

    Fix: Refactor this method to reduce its Cognitive Complexity from 17 to the 15 allowed.
    Issue ID: 7e0bf496-8932-4311-8d42-55bd1633d1a9
    Provider: SONARQUBE
    Severity: CRITICAL
    File: metadata/src/main/java/org/apache/kafka/metadata/KRaftMetadataCache.java
    Lines: 278-278

--- metadata/src/main/java/org/apache/kafka/metadata/KRaftMetadataCache.java
+++ metadata/src/main/java/org/apache/kafka/metadata/KRaftMetadataCache.java
@@ -287,49 +287,79 @@ public class KRaftMetadataCache implements MetadataCache {
         DescribeTopicPartitionsResponseData result = new DescribeTopicPartitionsResponseData();
         while (topics.hasNext()) {
             String topicName = topics.next();
-            if (remaining.get() > 0) {
-                var partitionResponseEntry = partitionMetadataForDescribeTopicResponse(image, topicName, listenerName, topicPartitionStartIndex.apply(topicName), remaining.get());
-                var partitionResponse = partitionResponseEntry.getKey();
-                int nextPartition = partitionResponseEntry.getValue();
-                if (partitionResponse.isPresent()) {
-                    List<DescribeTopicPartitionsResponsePartition> partitions = partitionResponse.get();
-                    DescribeTopicPartitionsResponseTopic response = new DescribeTopicPartitionsResponseTopic()
-                        .setErrorCode(Errors.NONE.code())
-                        .setName(topicName)
-                        .setTopicId(Optional.ofNullable(image.topics().getTopic(topicName).id()).orElse(Uuid.ZERO_UUID))
-                        .setIsInternal(Topic.isInternal(topicName))
-                        .setPartitions(partitions);
-                    result.topics().add(response);
-
-                    if (nextPartition != -1) {
-                        result.setNextCursor(new Cursor().setTopicName(topicName).setPartitionIndex(nextPartition));
-                        break;
-                    } else {
-                        remaining.addAndGet(-partitions.size());
-                    }
-                } else if (!ignoreTopicsWithExceptions) {
-                    Errors error;
-                    try {
-                        Topic.validate(topicName);
-                        error = Errors.UNKNOWN_TOPIC_OR_PARTITION;
-                    } catch (InvalidTopicException e) {
-                        error = Errors.INVALID_TOPIC_EXCEPTION;
-                    }
-                    result.topics().add(new DescribeTopicPartitionsResponseTopic()
-                        .setErrorCode(error.code())
-                        .setName(topicName)
-                        .setTopicId(getTopicId(topicName))
-                        .setIsInternal(Topic.isInternal(topicName)));
-                }
-            } else if (remaining.get() == 0) {
-                // The cursor should point to the beginning of the current topic. All the partitions in the previous topic
-                // should be fulfilled. Note that, if a partition is pointed in the NextTopicPartition, it does not mean
-                // this topic exists.
-                result.setNextCursor(new Cursor().setTopicName(topicName).setPartitionIndex(0));
+            if (!processTopicResponse(image, topicName, listenerName, topicPartitionStartIndex, remaining, result, ignoreTopicsWithExceptions)) {
                 break;
             }
         }
         return result;
+    }
+
+    private boolean processTopicResponse(
+        MetadataImage image,
+        String topicName,
+        ListenerName listenerName,
+        Function<String, Integer> topicPartitionStartIndex,
+        AtomicInteger remaining,
+        DescribeTopicPartitionsResponseData result,
+        boolean ignoreTopicsWithExceptions
+    ) {
+        if (remaining.get() > 0) {
+            return processTopicWithPartitions(image, topicName, listenerName, topicPartitionStartIndex, remaining, result, ignoreTopicsWithExceptions);
+        } else if (remaining.get() == 0) {
+            result.setNextCursor(new Cursor().setTopicName(topicName).setPartitionIndex(0));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean processTopicWithPartitions(
+        MetadataImage image,
+        String topicName,
+        ListenerName listenerName,
+        Function<String, Integer> topicPartitionStartIndex,
+        AtomicInteger remaining,
+        DescribeTopicPartitionsResponseData result,
+        boolean ignoreTopicsWithExceptions
+    ) {
+        var partitionResponseEntry = partitionMetadataForDescribeTopicResponse(image, topicName, listenerName, topicPartitionStartIndex.apply(topicName), remaining.get());
+        var partitionResponse = partitionResponseEntry.getKey();
+        int nextPartition = partitionResponseEntry.getValue();
+        if (partitionResponse.isPresent()) {
+            List<DescribeTopicPartitionsResponsePartition> partitions = partitionResponse.get();
+            DescribeTopicPartitionsResponseTopic response = new DescribeTopicPartitionsResponseTopic()
+                .setErrorCode(Errors.NONE.code())
+                .setName(topicName)
+                .setTopicId(Optional.ofNullable(image.topics().getTopic(topicName).id()).orElse(Uuid.ZERO_UUID))
+                .setIsInternal(Topic.isInternal(topicName))
+                .setPartitions(partitions);
+            result.topics().add(response);
+
+            if (nextPartition != -1) {
+                result.setNextCursor(new Cursor().setTopicName(topicName).setPartitionIndex(nextPartition));
+                return false;
+            } else {
+                remaining.addAndGet(-partitions.size());
+                return true;
+            }
+        } else if (!ignoreTopicsWithExceptions) {
+            addTopicErrorResponse(topicName, result);
+        }
+        return true;
+    }
+
+    private void addTopicErrorResponse(String topicName, DescribeTopicPartitionsResponseData result) {
+        Errors error;
+        try {
+            Topic.validate(topicName);
+            error = Errors.UNKNOWN_TOPIC_OR_PARTITION;
+        } catch (InvalidTopicException e) {
+            error = Errors.INVALID_TOPIC_EXCEPTION;
+        }
+        result.topics().add(new DescribeTopicPartitionsResponseTopic()
+            .setErrorCode(error.code())
+            .setName(topicName)
+            .setTopicId(getTopicId(topicName))
+            .setIsInternal(Topic.isInternal(topicName)));
     }
 
     @Override
