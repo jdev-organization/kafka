7f857796e606c4d7624f6c4aff3d3ef909351e8b
lucaspan <panxiangyi@hotmail.com>
2026-01-01 14:28:17 -0500

    Fix: Remove this 'public' modifier.
    Issue ID: 79e15f45-9ea9-4071-a274-958091258eda
    Provider: SONARQUBE
    Severity: LOW
    File: streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/MetricsIntegrationTest.java
    Lines: 347-347

--- streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/MetricsIntegrationTest.java
+++ streams/integration-tests/src/test/java/org/apache/kafka/streams/integration/MetricsIntegrationTest.java
@@ -383,7 +383,7 @@ public class MetricsIntegrationTest {
 
     @ParameterizedTest
     @ValueSource(booleans = {true, false})
-    public void shouldAddMetricsForWindowStoreAndSuppressionBuffer(final boolean streamsProtocolEnabled) throws Exception {
+    void shouldAddMetricsForWindowStoreAndSuppressionBuffer(final boolean streamsProtocolEnabled) throws Exception {
         if (streamsProtocolEnabled) {
             streamsConfiguration.put(StreamsConfig.GROUP_PROTOCOL_CONFIG, GroupProtocol.STREAMS.name().toLowerCase(Locale.getDefault()));
         }
